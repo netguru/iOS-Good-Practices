fa604e87f7c127c5c91eaf5cf20760e1b99a941c
Piotr Szadkowski <OfftheCode@gmail.com>
2022-02-21 10:07:33 +0100

    Create iOS Good Practices iOS app xcode project

---
+++ iOS Good Practices/iOS Good Practices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Good Practices
+//
+//  Created by Piotr Szadkowski on 21/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
