1a963097d4a2b30bb418f35d6d152a4a158e5834
Bartosz Dolewski <bartosz.dolewski@netguru.com>
2022-06-27 13:54:21 +0200

    Initial project for TCA

---
+++ Articles/The-Composable-Architecture/FoodTCA/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FoodTCA
+//
+//  Created by Bartosz Dolewski on 27/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Articles/The-Composable-Architecture/FoodTCA/FoodTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTCAApp.swift
+//  FoodTCA
+//
+//  Created by Bartosz Dolewski on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
