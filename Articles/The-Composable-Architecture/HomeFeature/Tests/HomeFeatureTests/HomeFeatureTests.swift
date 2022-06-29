80aaeda162af9c4ccb687619693cee8b6b077541
Bartosz Dolewski <bartosz.dolewski@netguru.com>
2022-06-29 11:06:03 +0200

    Tests for `HomeFeature`

--- Articles/The-Composable-Architecture/HomeFeature/Tests/HomeFeatureTests/HomeFeatureTests.swift
+++ Articles/The-Composable-Architecture/HomeFeature/Tests/HomeFeatureTests/HomeFeatureTests.swift
@@ -1,11 +1,88 @@
+//
+//  HomeFeatureTests.swift
+//
+//
+//  Created by Bartosz Dolewski on 27/06/2022.
+//
+
 import XCTest
+import Combine
 @testable import HomeFeature
+@testable import NetworkClient
+@testable import ComposableArchitecture
 
 final class HomeFeatureTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HomeFeature().text, "Hello, World!")
+    let scheduler = DispatchQueue.test
+    
+    func test_whenHomeScreenIsDisplayed_andNetworkResponseIsOK_nutritionFactsShouldBeAvailable() {
+        let sut = makeSUT(networkClient: .successful)
+        
+        sut.send(.download) {
+            $0.nutritionFacts = nil
+            $0.downloadingInProgress = true
+        }
+        
+        scheduler.advance()
+        
+        let expected = Food.map(from: .mock)
+        sut.receive(.receivedResponse(.success(expected))) {
+            $0.downloadingInProgress = false
+            $0.nutritionFacts = expected
+        }
+    }
+    
+    func test_whenHomeScreenIsDisplayed_andNetworkResponseIsBad_onlyAlertShouldBePresented() {
+        let sut = makeSUT(networkClient: .failable)
+        
+        sut.send(.download) {
+            $0.nutritionFacts = nil
+            $0.downloadingInProgress = true
+        }
+        
+        scheduler.advance()
+        
+        let expected = HomeScreenError.networkIssue("The operation couldn’t be completed. (NetworkClient.Failure error 1.)")
+        sut.receive(.receivedResponse(.failure(expected))) {
+            $0.downloadingInProgress = false
+            $0.nutritionFacts = nil
+            $0.alert = .init(
+                title: .init("Sorry, we have some trouble getting your nutrition facts: \(expected.localizedDescription)")
+            )
+        }
+    }
+    
+    func test_tappingMoreInfoButtonShouldPresentDetailsScreen_and_swipingDownShouldDismissDetailsScreen() {
+        let sut = makeSUT(networkClient: .successful)
+        
+        sut.send(.showDetails) {
+            $0.presentingDetails = true
+        }
+        
+        sut.send(.dismissDetails) {
+            $0.presentingDetails = false
+        }
+        scheduler.advance()
     }
 }
+
+extension HomeFeatureTests {
+    func makeSUT(
+        initialState: HomeScreenState = .init(),
+        networkClient: NetworkClient
+    ) -> TestStore<HomeScreenState, HomeScreenState, HomeScreenAction, HomeScreenAction, HomeScreenEnvironment> {
+
+        let testEnvironment = HomeScreenEnvironment(
+            networkClient: networkClient,
+            mainQueue: self.scheduler.eraseToAnyScheduler()
+        )
+        
+        let store = TestStore(
+            initialState: HomeScreenState(),
+            reducer: homeScreenReducer,
+            environment: testEnvironment
+        )
+        
+        return store
+    }
+}
+
