582bca3bf592a68fd5cc32a9265b507bffe7cec2
Jolanta Zakrzewska <73111502+jolantazakrzewska-netguru@users.noreply.github.com>
2022-08-16 08:44:25 +0200

    Identity pinning test project

---
+++ Articles/General/IdentityPinningTest/IdentityPinningTest/Extensions/UIScreen.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen.swift
+//  IdentityPinningTest
+//
+//  Created by Jolanta Zakrzewska on 10/08/2022.
+//
+
+import SwiftUI
+
+extension UIScreen {
+    static var width: CGFloat {
+        main.bounds.width
+    }
+}
